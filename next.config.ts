a8c57ac21b095dccc1459c69ed872a87278674a4
lunargravity <altankhanu@gmail.com>
2025-08-05 17:01:44 -0400

    Fix basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/anuromance"
+  basePath: "/anuromance",
+  assetPrefix: "/anuromance"
 };
 
 export default nextConfig;
