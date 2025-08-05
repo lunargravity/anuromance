cb0f37b757619fb1d8233e803f4c8c61d8ae3278
lunargravity <altankhanu@gmail.com>
2025-08-05 11:15:01 -0400

    background night sky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,63 @@
-import Image from "next/image";
+"use client";
+import React, { useRef, useEffect } from "react";
+import styles from "./stars.module.css";
 
-export default function Home() {
+function random(range: number, unit: string) {
+  const randNum = Math.floor(Math.random() * range) + 1;
+  return `${randNum}${unit}`;
+}
+
+function createStar(size: number, width: number, height: number) {
+  const randRange5 = Math.floor(Math.random() * 5) + 1;
+  const widthAndHeight = random(size, "px");
+  return {
+    className: `${styles.circle} ${styles[`blink_${randRange5}`]}`,
+    style: {
+      width: widthAndHeight,
+      height: widthAndHeight,
+      left: random(width, "px"),
+      top: random(height, "px"),
+      position: "absolute" as const,
+    },
+  };
+}
+
+export default function Page() {
+  const [stars, setStars] = React.useState<any[]>([]);
+  const skyRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const starArr = [];
+    for (let i = 0; i < 50; i++) {
+      starArr.push(createStar(5, width, height));
+    }
+    setStars(starArr);
+  }, []);
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <section>
+        <div
+          className={styles.sky}
+          ref={skyRef}
+          style={{ position: "relative", width: "100vw", height: "100vh" }}
+        >
+          {stars.map((star, idx) => (
+            <div key={idx} className={star.className} style={star.style}></div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </section>
+      <div className="mockup-window border border-base-300 w-full">
+        <div className="grid place-content-center border-t border-base-300 h-80">
+          Hello!
+        </div>
+      </div>
+    </>
   );
 }
