03627cd68833501eefaae630ec45781ff76224e7
lunargravity <altankhanu@gmail.com>
2025-08-07 15:54:04 -0400

    Added draggable container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,49 @@ type Star = {
   style: React.CSSProperties;
 };
 
+function dragElement(elmnt: HTMLElement) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById("draggable-header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById("draggable-header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+  function dragMouseDown(e: MouseEvent) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e: MouseEvent) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 export default function Page() {
   const [stars, setStars] = React.useState<Star[]>([]);
   const skyRef = useRef<HTMLDivElement>(null);
@@ -43,6 +86,11 @@ export default function Page() {
       starArr.push(createStar(5, width, height));
     }
     setStars(starArr);
+
+    const container = document.getElementById("draggable-container")!;
+    if (container) {
+      dragElement(container);
+    }
   }, []);
 
   return (
@@ -63,7 +111,7 @@ export default function Page() {
         <span className={styles.shootingStar}></span>
         <div className="mockup-window">
           <div className="home">
-            <div className="bar">home</div>
+            <div className="bar">Anu Altankhuyag</div>
             <div className="first-row">
               <Image
                 src={`${basePath}/profile.jpg`}
@@ -73,15 +121,24 @@ export default function Page() {
                 height={100}
               />
               <div className="custom-container">
-                <p className="logline-label">
-                  LOGLINE
-                </p>
+                <p className="logline-label">logline</p>
                 <div>
-                  <p className="logline">Ghostwriter with a BFA in Creative Writing, delivering romance stories that balance fresh emotions with classic tropes, turning every project into a new romance worth falling for.</p>
-                  <button className="contact">Let&apos;s Connect</button>
+                  <p className="logline">
+                    Ghostwriter with a BFA in Creative Writing, delivering
+                    romance stories that balance fresh emotions with classic
+                    tropes, turning every project into a new romance worth
+                    falling for.
+                  </p>
+                  <button className="contact">let&apos;s connect</button>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div id="draggable-container" className="draggable-container">
+          <div id="draggable-header" className="draggable-header">Contact</div>
+          <div className="contact-info">
+            <p>Email: altankhanu@gmail.com</p>
           </div>
         </div>
       </section>
