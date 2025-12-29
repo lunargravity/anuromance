0e04439e194746db3bac4cac790484963a27634b
lunargravity <altankhanu@gmail.com>
2025-12-29 07:02:58 -0500

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,35 @@
-import type { Metadata } from 'next'
-import './global.css'
- 
+import type { Metadata } from "next";
+import "./global.css";
+
 export const metadata: Metadata = {
-  title: 'Anu Romance Portfolio',
-  description: 'My personal portfolio showcasing my writing and creative work.',
-}
- 
+  title: "Anu Romance Portfolio",
+  description: "My personal portfolio showcasing my writing and creative work.",
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
